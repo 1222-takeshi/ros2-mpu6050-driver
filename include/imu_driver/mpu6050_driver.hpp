3f193890ef03a0de6769f74205a6f40242e759a3
1222-takeshi <m.takeshi1995@gmail.com>
2021-12-02 13:42:59 +0000

    initial commit

---
+++ include/imu_driver/mpu6050_driver.hpp
@@ -0,0 +1,39 @@
+// Copyright 2021 Takeshi Miura
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef IMU_DRIVER__MPU6050_DRIVER_HPP_
+#define IMU_DRIVER__MPU6050_DRIVER_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+
+#include <memory>
+#include <string>
+#include <iostream>
+
+class Mpu6050Driver : public rclcpp::Node
+{
+public:
+  Mpu6050Driver(const std::string & node_name, const rclcpp::NodeOptions & options);
+
+private:
+  void onTimer();
+  void updateCurrentIMUData();
+  float get2data(int fd, unsigned int reg);
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Clock::SharedPtr clock_;
+};
+
+#endif  // IMU_DRIVER__MPU6050_DRIVER_HPP_
